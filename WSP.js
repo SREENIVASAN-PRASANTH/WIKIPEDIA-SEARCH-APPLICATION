4cd403178af5e017d358d206f5176b97d1e1f0dc
S PRASANTH <122166617+SREENIVASAN-PRASANTH@users.noreply.github.com>
2023-10-30 20:06:55 +0530

    Create WSP.js
    javascript file

---
+++ WSP.js
@@ -0,0 +1,66 @@
+let searchInputEl = document.getElementById("searchInput");
+let searchResultsEl = document.getElementById("searchResults");
+let spinnerEl = document.getElementById("spinner");
+
+function createAndAppendSearchResult(result) {
+    let resultItemEl = document.createElement("div");
+    resultItemEl.classList.add("result-item");
+    searchResultsEl.appendChild(resultItemEl);
+    let {
+        link,
+        title,
+        description
+    } = result;
+    let titleEl = document.createElement("a");
+    titleEl.href = link;
+    titleEl.target = "_blank";
+    titleEl.textContent = title;
+    titleEl.classList.add("result-title");
+    resultItemEl.appendChild(titleEl);
+    let titleBreakEl = document.createElement("br");
+    resultItemEl.appendChild(titleBreakEl);
+    let urlEl = document.createElement("a");
+    urlEl.classList.add("result-url");
+    urlEl.href = link;
+    urlEl.target = "_blank";
+    urlEl.textContent = link;
+    resultItemEl.appendChild(urlEl);
+    let linkBreakEl = document.createElement("br");
+    resultItemEl.appendChild(linkBreakEl);
+    let descriptionEl = document.createElement("p");
+    descriptionEl.classList.add("link-description");
+    descriptionEl.textContent = description;
+    resultItemEl.appendChild(descriptionEl);
+}
+
+function displayResults(searchResults) {
+    spinnerEl.classList.toggle("d-none");
+    for (let result of searchResults) {
+        createAndAppendSearchResult(result);
+    }
+}
+
+
+function wikipediaSearch(event) {
+    if (event.key === "Enter") {
+        spinnerEl.classList.toggle("d-none");
+        searchResultsEl.textContent = "";
+        let searchInput = searchInputEl.value;
+        let URL = "https://apis.ccbp.in/wiki-search?search=" + searchInput;
+        let options = {
+            method: "GET"
+        }
+        fetch(URL, options)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(jsonData) {
+                let {
+                    search_results
+                } = jsonData;
+                displayResults(search_results);
+
+            })
+    }
+}
+searchInputEl.addEventListener("keydown", wikipediaSearch);
